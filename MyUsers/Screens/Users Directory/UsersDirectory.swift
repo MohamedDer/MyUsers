3e1e142b9c3961568f964576b7f2aa32dcb37f68
Mohamed <mde@frianbiz.com>
2020-03-05 00:32:02 +0100

    WIP Implement main screen

--- MyUsers/Screens/Users Directory/UsersDirectory.swift
+++ MyUsers/Screens/Users Directory/UsersDirectory.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
 
 class UsersDirectoryViewController: UIViewController {
+    
+    var userViewModel = UsersViewModel()
+    public var users = PublishSubject<[User]>()
+    let disposeBag = DisposeBag()
+
+    var testV = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let uvm = UsersViewModel()
-        uvm.fetchUsers()
+
+        // Setup views
+        
+        testV.bounds = self.view.bounds
+        testV.backgroundColor = .red
+        view.addSubview(testV)
+        let usersNumberLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 100))
+        usersNumberLabel.backgroundColor = .yellow
+        usersNumberLabel.text = "dada"
+        self.view.addSubview(usersNumberLabel)
+        
+        //Setup Binding
+
+        userViewModel.loading
+            .bind(to: self.testV.rx.isHidden).disposed(by: disposeBag)
+        
+        
+        userViewModel
+                   .error
+                   .observeOn(MainScheduler.instance)
+                   .subscribe(onNext: { (error) in
+                        print("dadada"+error)
+                   })
+                   .disposed(by: disposeBag)
+        
+        userViewModel
+            .users
+            .observeOn(MainScheduler.instance)
+            .bind(to: users)
+            .disposed(by: disposeBag)
+    
+    
+        userViewModel.fetchUsers()
     }
 
 
