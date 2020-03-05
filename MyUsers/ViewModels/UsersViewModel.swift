2c5b6c176d090e8efdb79a4d290a34d49949c866
Mohamed <mde@frianbiz.com>
2020-03-05 16:44:32 +0100

    Add timer and append new data

--- MyUsers/ViewModels/UsersViewModel.swift
+++ MyUsers/ViewModels/UsersViewModel.swift
@@ -18,24 +18,32 @@ class UsersViewModel {
             public let users : PublishSubject<[User]> = PublishSubject()
             public let loading: PublishSubject<Bool> = PublishSubject()
             public let error : PublishSubject<String> = PublishSubject()
+            private var _users = [User]()
             
             private let disposable = DisposeBag()
 
             public func fetchUsers(){
-                self.loading.onNext(true)
-                let params = ["results": 10]
-                AF.request("https://randomuser.me/api", method: .get, parameters: params ).responseJSON { (response) in
-                    switch response.result {
-                    case .success:
-                        let usersJsonObject = (response.value as? [String:Any])?["results"]
-                        if let users = Mapper<User>().mapArray(JSONObject: usersJsonObject) {
-                            self.loading.onNext(false)
-                            self.users.onNext(users)
-                        }
-                    case let .failure(error):
-                        self.error.onNext(error.localizedDescription)
+                
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                    
+                    self.loading.onNext(true)
+                    let params = ["results": 10]
+                    AF.request("https://randomuser.me/api", method: .get, parameters: params ).responseJSON { (response) in
+                        switch response.result {
+                        case .success:
+                            let usersJsonObject = (response.value as? [String:Any])?["results"]
+                            if let users = Mapper<User>().mapArray(JSONObject: usersJsonObject) {
+                                self.loading.onNext(false)
+                                self._users.append(contentsOf: users)
+                                self.users.onNext( self._users)
+                            }
+                        case let .failure(error):
+                            self.error.onNext(error.localizedDescription)
 
+                        }
                     }
                 }
+                
+                
             }
 }
